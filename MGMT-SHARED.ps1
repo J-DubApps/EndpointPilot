ac0975cb60f3f6ef76b874806740e5ad63769f80
Julian West <west.julian@me.com>
2025-09-01 20:56:44 -0500

    schema changes

--- MGMT-SHARED.ps1
+++ MGMT-SHARED.ps1
@@ -67,6 +67,20 @@ Function Get-OperatingSystemApproved() {
     }
 }
 
+# Test if an operation requires elevation
+function Test-RequiresElevation {
+    param($operation)
+    
+    # Check explicit admin flag
+    if ($operation.requiresAdmin -eq $true) { return $true }
+    
+    # Auto-detect based on operation type
+    if ($operation.path -like "HKLM*") { return $true }
+    if ($operation.destinationPath -like "$env:ProgramFiles*") { return $true }
+    if ($operation.destinationPath -like "$env:SystemRoot*") { return $true }
+    
+    return $false
+}
 
 Function Copy-Directory($Path, $NewPath) {
     ##########################################################################
